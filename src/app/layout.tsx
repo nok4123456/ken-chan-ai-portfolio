a359008688f073bf1dd151d75f897c0dc9935705
Ken Chan <nok4123456@gmail.com>
2024-10-09 12:41:57 +0800

    Change favicon and title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,22 @@
 import Navbar from "./components/Navbar";
 import "./globals.css";
 import Footer from "./components/Footer";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Ken Chan Portfolio",
+  description:
+    "Welcome to Ken Chan's professional portfolio showcasing skills, projects, and experiences.",
+  icons: {
+    icon: [
+      { url: "/favicon.ico" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    ],
+    apple: [{ url: "/apple-touch-icon.png" }],
+  },
+};
+
 export default function RootLayout({
   children,
 }: {
